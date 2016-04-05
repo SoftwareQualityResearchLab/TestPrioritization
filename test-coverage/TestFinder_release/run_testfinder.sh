3f3989bb422132a2a8c1b4d9c08cda196ddf59f5
Mostafa Mahdieh <mahdieh@viratech.ir>
2016-04-05 18:34:21 +0430

    added path to run_testfinder.sh

--- test-coverage/TestFinder_release/run_testfinder.sh
+++ test-coverage/TestFinder_release/run_testfinder.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-	echo "usage: run_testfinder.sh [main-test-package]"
+	echo "usage: run_testfinder.sh [absolute-project-path] [main-test-package]"
 	echo "main-test-package example: org.apache.commons.math3"
 	exit
 fi
 
-java -cp target/classes:target/test-classes:TestFinder.jar:lib/* org.testfinder.TestFinder $1
+java -cp $1/target/classes:$1/target/test-classes:TestFinder.jar:lib/* org.testfinder.TestFinder $2
